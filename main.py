8ab7778a286e96b11aecda82f8415d8cd9a0c84b
Kwon770 <sckwon770@gmail.com>
2022-02-01 03:45:32 +0900

    fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from os.path import isfile, join
+from pathlib import Path
 import sys
 import re
 from shutil import copyfile
@@ -18,7 +19,11 @@ ______      _          _____      _   _
 날짜를 입력할 파일 목록 입니다."""
 EXCEL_EXTENSIONS = ["xlsx", "xlsm", "xlsb", "xltx", "xls", "xlt", "xml", "xlam", "xlw", "xlr"]
 
+current_path = ""
 excel_files_len = 0
+
+def pause_console():
+    os.system('pause')
 
 def clear_console():
     os.system('cls')
@@ -31,10 +36,10 @@ def print_program_announcement():
 
 def scan_directory():
     global EXCEL_EXTENSIONS
+    global current_path
     global excel_files_len
 
-    # current_path = sys.path[0]
-    current_path = '/'.join(sys.executable.split('/')[:-1])
+    current_path = Path.cwd()
     excel_file_list = []
     for f in os.listdir(current_path):
         if not isfile(join(current_path, f)):
@@ -45,23 +50,20 @@ def scan_directory():
             continue
 
         file_info2 = re.split('[-.]', f)
-        if len(file_info2) >= 3 and file_info2[1].isnumeric() and file_info2[2].isnumeric():
+        if len(file_info2) >= 3 and file_info2[-2].isnumeric() and file_info2[-3].isnumeric():
             continue
 
         excel_file_list.append(f)
 
     excel_files_len = len(excel_file_list)
-    if excel_files_len == 0:
-        print("[시스템] 변경할 파일이 없습니다.")
-        sys.exit(0)
-
     return excel_file_list
 
 
 
 def print_files_list(files_list):
+    print('- 0. 파일 목록 새로고침')
     for idx, file in enumerate(files_list):
-        print(f'- {idx}. {file}')
+        print(f'- {idx+1}. {file}')
     print()
 
 
@@ -72,29 +74,48 @@ def input_target_index():
     while True:
         try:
             target_index = int(input("[입력] 변경할 파일의 번호를 입력하세요 : "))
-            if target_index >= excel_files_len:
+            if target_index > excel_files_len:
                 raise ValueError
 
-            return target_index
+            return target_index-1
 
         except ValueError:
             print("[에러] 올바르지 않은 입력 형태입니다.")
 
 
-def set_date_on_file(files_list, target_index):
-    target_file = files_list[target_index]
+def validate_target_file(target_file):
+    target_file_name, target_file_extension = target_file.split('.')
+    date_string = date.today().strftime("%y-%m-%d")
+    date_setter_name = target_file_name + " " + date_string + "." + target_file_extension
+    for f in os.listdir(current_path):
+        if f == date_setter_name:
+            return False
+
+    return True
+
+def set_date_on_file(target_file):
     target_file_name, target_file_extension = target_file.split('.')
     copyfile(target_file, "TEMP")
 
     date_string = date.today().strftime("%y-%m-%d")
-    os.rename("TEMP", target_file_name + date_string + "." + target_file_extension)
+    os.rename("TEMP", target_file_name + " " + date_string + "." + target_file_extension)
 
 
 if __name__ == '__main__':
-    print_program_announcement()
+    while True:
+        print_program_announcement()
 
-    files_list = scan_directory()
-    print_files_list(files_list)
+        files_list = scan_directory()
+        print_files_list(files_list)
 
-    target_index = input_target_index()
-    set_date_on_file(files_list, target_index)
+        target_index = input_target_index()
+        if target_index == -1:
+            continue
+
+        target_file = files_list[target_index]
+        if not validate_target_file(target_file):
+            print("[시스템] 중복된 파일이 있습니다.")
+            pause_console()
+            continue
+
+        set_date_on_file(target_file)
